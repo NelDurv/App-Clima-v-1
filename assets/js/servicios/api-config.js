6e72672b653eb5d963ab484ff7c30d58c2b30812
NelDurv <noslen.dur@gmail.com>
2026-05-19 00:30:46 -0400

    carpetas servicios clima

---
+++ assets/js/servicios/api-config.js
@@ -0,0 +1,3 @@
+/* # Configuración de API */
+const API_KEY = "b1b15e88fa797225412429c5c50c122a1";
+const API_BASE_URL = "https://api.openweathermap.org/data/2.5";
---
+++ assets/js/servicios/weather-service.js
@@ -0,0 +1,3 @@
+/* # Servicio de clima*/    
+const url = "https://api.openweathermap.org/data/2.5/weather?q=";
+const API_KEY = "b1b15e88fa797225412429c5c50c122a1";
